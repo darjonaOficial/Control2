ef09bbb43ec45fdf7031fd5fdc95de77206e0098
darjonaOficial <darjona@martipous.cat>
2025-01-25 19:29:54 +0100

    Files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "pxt.h"
+
+namespace control {
+    /**
+    * Gets the source of the last event executed on the bus
+    */
+    //% blockId="control_event_source" block="event source"
+    //% help=control/event-source
+    //% weight=18
+    int eventSource() {
+        return pxt::lastEvent.source;
+    }
+}
